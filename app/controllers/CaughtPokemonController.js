1b549d6cf9783e7ddb958654b90791dcf5ec3c28
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-10-12 16:43:11 -0600

    error handle

--- app/controllers/CaughtPokemonController.js
+++ app/controllers/CaughtPokemonController.js
@@ -31,6 +31,7 @@ export class CaughtPokemonController {
 
     setActivePokemon(pokemonId) {
         caughtPokemonService.setActivePokemon(pokemonId)
+        // @ts-ignore
         bootstrap.Offcanvas.getOrCreateInstance('#my-pokemon-canvas').hide()
     }
 
